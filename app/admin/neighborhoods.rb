78236d6ec5f0e8e9d3e9d1511e1cdc5bb721e7a0
Raghu Betina <raghu@firstdraft.com>
2018-04-01 23:08:21 +0000

    Whitelist columns in Neighborhood for ActiveAdmin

--- app/admin/neighborhoods.rb
+++ app/admin/neighborhoods.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Neighborhood do
+
+ permit_params :venue_count, :name, :city, :state
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
