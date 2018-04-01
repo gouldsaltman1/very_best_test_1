aa91d86a68fbb6b04cf2082b96705e87af00ea1d
Raghu Betina <raghu@firstdraft.com>
2018-04-01 23:07:49 +0000

    Validate uniqueness of bookmarks.venue_id

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,6 +5,8 @@ class Bookmark < ApplicationRecord
 
   # Validations
 
+  validates :venue_id, :uniqueness => { :scope => [:user_id, :dish_id], :message => "already bookmarked" }
+
   validates :venue_id, :presence => true
 
 end
