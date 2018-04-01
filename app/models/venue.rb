eb1a20f695973c2fd905690206b71db65aa71d8e
Raghu Betina <raghu@firstdraft.com>
2018-04-01 23:07:49 +0000

    Validate uniqueness of venues.neighborhood_id

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,6 +5,8 @@ class Venue < ApplicationRecord
 
   # Validations
 
+  validates :neighborhood_id, :uniqueness => { :scope => [:address, :venue_name] }
+
   validates :venue_name, :uniqueness => { :scope => [:address] }
 
   validates :venue_name, :presence => true
